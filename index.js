f5edee7d81d46686a59f51eb44f135042cd25e76
rakibhasan197 <alhasanmdrakib062@gmail.com>
2026-06-08 17:02:16 +0600

    added commet api

--- index.js
+++ index.js
@@ -28,26 +28,7 @@ const JWKS = createRemoteJWKSet(
   new URL('http://localhost:3000/api/auth/jwks')
 )
 
-const verifyToken = async (req, res, next)=> {
-       const authHeader = req?.headers.authorization
-       if(!authHeader){
-        return res.status(401).json({ message: "Unauthorized" });
-       }
-       const token = authHeader.split(" ")[1]
-       if(!token){
-        return res.status(401).json({ message: "Unauthorized" });
-       }
 
-      try{
-         const {payload} = await jwtVerify(token, JWKS)
-         console.log(payload)
-          next()
-      } catch (error){
-        return res.status(403).json({ message: "Forbidden" });
-      }
-       
-      
-    }
 
 
 async function run() {
@@ -95,6 +76,64 @@ async function run() {
   res.send(result);
 });
 
+app.get('/comments', verifyToken, async (req, res) => {
+  const comments = await commentsCollection.find().toArray();
+  res.send(comments);
+});
+
+    app.get('/comments/:ideaId', async (req, res) => {
+  const { ideaId } = req.params;
+
+  const comments = await commentsCollection
+    .find({ ideaId })
+    .sort({ createdAt: -1 })
+    .toArray();
+
+  res.send(comments);
+});
+
+
+app.get("/my-ideas", verifyToken, async (req, res) => {
+  const { email } = req.query;
+
+  if (!email) {
+    return res.status(400).send({ message: "Email is required" });
+  }
+
+  const result = await ideasCollection
+    .find({ userEmail: email })
+    .sort({ createdAt: -1 })
+    .toArray();
+
+  res.send(result);
+});
+
+
+    app.post('/comments', async (req, res)=>{
+      const comment = req.body;
+      console.log(comment);
+      const result = await commentsCollection.insertOne(comment);
+      res.send(result);
+    })
+
+
+    app.patch('/comments/:id', async (req, res) => {
+  const { id } = req.params;
+  const { comment } = req.body;
+
+  const result = await commentsCollection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: { comment} }
+  );
+  res.send(result);
+});
+
+
+  app.delete('/comments/:id', async (req, res)=>{
+    const {id} = req.params;
+    const result = await commentsCollection.deleteOne({_id: new ObjectId(id)});
+    res.send(result);
+  })
 
     
 
